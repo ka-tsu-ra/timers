7f899b145209a07d0e1cf9db7500d535860d369e
katsuraku <kirsten.jones@ft.com>
2016-02-28 14:42:36 +0000

    React, redux, webpack setup, basic html page

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import {render} from 'react-dom';
-import AwesomeComponent from './AwesomeComponent.jsx';
 
 class App extends React.Component {
   render () {
     return (
       <div>
-        <p>Yo make some dots</p>
-        <AwesomeComponent />
+        <p>My timers</p> 
       </div>
     )
   }
