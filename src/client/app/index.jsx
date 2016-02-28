4058cc8a86508a7a347399fa70ac32cd52417b00
katsuraku <kirsten.jones@ft.com>
2016-02-28 17:56:05 +0000

    Renders timer initial state. Clicking increments time in console. WIP connecting UI to updatewhen time state changes

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,14 +1,39 @@
 import React from 'react';
-import {render} from 'react-dom';
+import { render } from 'react-dom';
+import { createStore } from 'redux';
+
+// Reducer function.
+// For now, the representation of time passing will just be a number that you manually increment.
+const timer = (state = 0, action) => {
+  if (action.type === 'INCREMENT') {
+    return state + 1;
+  } 
+  else {
+    return state;
+  } 
+};
+// Create the Redux store, and specify that timer is the reducer function that will determine what happens to the state.
+const store = createStore(timer);
+store.subscribe(() => {
+  console.log(store.getState())
+});
+
+document.addEventListener('click', () => {
+  store.dispatch( { type: 'INCREMENT' });
+});
 
 class App extends React.Component {
+
   render () {
+    let timePassed = store.getState();
     return (
       <div>
-        <p>My timers</p> 
+        <h1>My timer:</h1>
+        <p>{timePassed}</p>	
       </div>
     )
   }
 }
-
 render(<App/>, document.getElementById('app'));
+
+
