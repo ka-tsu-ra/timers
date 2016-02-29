89b559c901f539af8a4d533e63bcc50364f11708
katsuraku <kirsten.jones@ft.com>
2016-02-29 23:18:05 +0000

    WIP getting UI to render updates in store triggere by button click

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,39 +1,62 @@
 import React from 'react';
-import { render } from 'react-dom';
 import { createStore } from 'redux';
-
-// Reducer function.
-// For now, the representation of time passing will just be a number that you manually increment.
-const timer = (state = 0, action) => {
-  if (action.type === 'INCREMENT') {
-    return state + 1;
-  } 
-  else {
-    return state;
-  } 
-};
-// Create the Redux store, and specify that timer is the reducer function that will determine what happens to the state.
-const store = createStore(timer);
-store.subscribe(() => {
-  console.log(store.getState())
-});
-
-document.addEventListener('click', () => {
-  store.dispatch( { type: 'INCREMENT' });
-});
+// import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
   render () {
     let timePassed = store.getState();
     return (
       <div>
         <h1>My timer:</h1>
-        <p>{timePassed}</p>	
+        <p>{timePassed}</p>
+	      <button onClick={() => {
+      	  store.dispatch({
+      	    type: 'INCREMENT'
+      	  })
+      	}}>
+    	  Add time
+      	</button>
       </div>
-    )
+    );
   }
 }
+
+// Reducer function.
+// For now, the representation of time passing will just be a number that you manually increment.
+const timer = (state = 0, action) => {
+  if (action.type === 'INCREMENT') {
+    return state + 1;
+  }
+  else {
+    return state;
+  }
+};
+
+// const render = () => {
+//   ReactDOM.render(
+//     <App
+//       timePassed={store.getState()}
+//     />,
+//     document.getElementById('app')
+//     );
+// };
+
+// Create the Redux store, and specify that timer is the reducer function that will determine what happens to the state.
+const store = createStore(timer);
+store.subscribe(render);
+
+
+//store.subscribe() => {
+//  console.log(store.getState())
+//};
+
+//document.addEventListener('click', () => {
+//  store.dispatch({ type: 'INCREMENT' });
+//});
+
 render(<App/>, document.getElementById('app'));
-
-
